7a36bf3d51b6884a89d82d297176f4cc6710d232
laurent <laurent.pose@orange.com>
2020-09-07 10:52:13 +0200

    dockercolor

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,6 @@ alias vim /usr/bin/nvim
 alias screen "env TERM=screen-256color-it screen"
 alias ccat "highlight -O ansi --force"
 alias dct "docker-compose exec alias /opt/alias/phpunit"
+alias di "/root/dotfile/dockercolor/docker-images"
+alias dps "/root/dotfile/dockercolor/docker-ps"
+alias dcps "/root/dotfile/dockercolor/docker-compose-ps"
