775fd89ea5d09549a3fbf1ef03167403a2c2ac15
laurent <laurent.pose@orange.com>
2020-09-07 10:45:40 +0200

    dockercolor

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,5 @@
-set -x TERM xterm-256color
+set -x TERM xterm-256color-it
 alias vim /usr/bin/nvim
 alias screen "env TERM=screen-256color-it screen"
+alias ccat "highlight -O ansi --force"
+alias dct "docker-compose exec alias /opt/alias/phpunit"
