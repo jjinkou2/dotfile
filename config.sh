6e81275541070a9e47a4bd0225ba659256b85c21
jjinkou2 <jjinkou2@yahoo.fr>
2020-11-05 13:11:32 +0100

    add kitty conf

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 ln -s ~/dotfile/SpaceVim.d/ ~/.SpaceVim.d
 ln -s ~/dotfile/config/omf/ ~/.config/omf
 ln -s ~/dotfile/config/fish/ ~/.config/fish
+ln -s ~/dotfile/config/kitty/ ~/.config/kitty
 ln -s ~/dotfile/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/dotfile/tmux/tmux-themepack ~/.tmux-themepack
