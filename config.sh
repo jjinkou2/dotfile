5da26c299b7e098f053352d3ccfbe8a91ead780a
laurent pose <laurent.pose@orange.com>
2021-04-12 09:15:15 +0200

    test

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@ ln -s ~/dotfile/SpaceVim.d/ ~/.SpaceVim.d
 ln -s ~/dotfile/config/omf/ ~/.config/omf
 ln -s ~/dotfile/config/fish/ ~/.config/fish
 ln -s ~/dotfile/config/kitty/ ~/.config/kitty
-ln -s ~/dotfile/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfile/tmux/tmux.conf ~/.tmux.conf 
 ln -s ~/dotfile/tmux/tmux-themepack ~/.tmux-themepack
